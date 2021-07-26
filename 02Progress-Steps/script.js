9cd79c9ca77984d2574839e613a7960fffa1bf3e
永乐 <yl153977@163.com>
2021-07-26 20:12:51 +0800

    02 finished

--- 02Progress-Steps/script.js
+++ 02Progress-Steps/script.js
@@ -1,3 +1,32 @@
+let current=1
 const prev=document.querySelector('#prev')
+const next=document.querySelector('#next')
+const circles=document.querySelectorAll('.circle')
+const progress=document.querySelector('#progress')
 
-prev.addEventListener('onmousedown', function (){prev.style.fontsize='13px'})
+prev.addEventListener('click',function (){
+    current-=1
+    flush()
+})
+next.addEventListener('click', function () {
+    current+=1
+    flush()
+})
+
+function flush() {
+    if (current==1){
+        prev.disabled=true
+    }else if (current==4){
+        next.disabled=true
+    }else{
+        next.disabled=false
+        prev.disabled=false
+    }
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].classList.remove('active')
+    }
+    for (let i = 0; i < current; i++) {
+        circles[i].classList.add('active')
+    }
+    progress.style.width=133 * (current-1) + 'px'
+}
