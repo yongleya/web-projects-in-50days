72b6e75111f968ab3f509466088a02f958f4e1d1
永乐 <yl153977@163.com>
2021-07-27 23:20:35 +0800

    05 finished

---
+++ 05Blurry-Loading/script.js
@@ -0,0 +1,15 @@
+window.onload=function () {
+    let times=0
+    const background_item=document.querySelectorAll('.background')[0]
+    const progress=document.querySelectorAll('.progress')[0]
+    var my_var = setInterval(function (){
+        progress.innerHTML=10+times*3+ "%"
+        progress.style.opacity=1-times/30
+        background_item.style.filter='blur('+(30-times)+'px)'
+        times+=1
+        if (times==31){
+            clearInterval(my_var)
+        }
+    },50)
+
+}
